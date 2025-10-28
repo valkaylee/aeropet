decd1ce30836a900a3ae0c0fcf56f3728df24238
hkaylee <kayleeh@usc.edu>
2025-10-27 21:38:29 -0700

    up down left right forward backward

--- SDK_link_test.py
+++ SDK_link_test.py
@@ -1,8 +1,40 @@
 from djitellopy import Tello
+import time
 
-t = Tello()
-t.connect()
-print(t.get_battery())
-t.takeoff()
-t.land()
-t.end()
+# Initialize the drone
+drone = Tello()
+drone.connect()
+
+# Print battery percentage
+print(f"Battery: {drone.get_battery()}%")
+
+# Take off
+drone.takeoff()
+time.sleep(2)
+
+# Move forward 50cm
+drone.move_forward(50)
+time.sleep(2)
+
+# Move backward 50cm
+drone.move_back(50)
+time.sleep(2)
+
+# Move up 50cm
+drone.move_up(50)
+time.sleep(2)
+
+# Move down 50cm
+drone.move_down(50)
+time.sleep(2)
+
+# Move left 50cm
+drone.move_left(50)
+time.sleep(2)
+
+# Move right 50cm
+drone.move_right(50)
+time.sleep(2)
+
+# Land
+drone.land()
